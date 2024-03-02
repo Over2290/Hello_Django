588c71587272a4c1bcb4c799372ace42c12dfc8a
User <denisserbakov990@gmail.com>
2024-03-02 23:45:27 +0300

    start_files

---
+++ hello_django/hello_django/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+def about(request):
+    return render(request,'about.html',{'gretting':'hello'})
+def home(request):
+    return HttpResponse('This is my home')
